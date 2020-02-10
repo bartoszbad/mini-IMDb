c929d15fce293acaaffc923aa85e99588a4dd43a
Bartosz Badowski <bartoszbad@gmail.com>
2020-02-10 23:45:34 +0100

    test added for views: RateMovie, RateMovieDetail

--- omdbapi/tests.py
+++ omdbapi/tests.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import RequestsClient, APIRequestFactory
 
-from omdbapi.models import Movie
+from omdbapi.models import Movie, MovieRating
 from omdbapi.serializers import MovieSerializer
 
 client = RequestsClient()
@@ -20,7 +20,7 @@ class TestMovieListView(TestCase):
         response = self.client.get('/movies/')
         self.assertEqual(response.status_code, 200)
 
-    def test_number_of_documents(self):
+    def test_number_of_movies(self):
         self.assertEqual(Movie.objects.count(), 2)
 
 
@@ -105,22 +105,106 @@ class TestMoviesByTitle(TestCase):
         self.assertEqual(response.status_code, 201)
 
 
-class TestFavouritesMoviesByAnonymous(TestCase):
-    def test_request_client(self):
-        response = self.client.get('/favourites-movies/')
-        self.assertEqual(response.status_code, 403)
-
-
 class TestFavouritesMoviesByUser(TestCase):
     def setUp(self):
         self.username = 'testtest'
         self.password = 'password123'
         self.user = User.objects.create_user(self.username, self.password)
-        self.client.force_login(user=self.user)
 
-    def test_request_client(self):
+    def test_request_anonymous_user(self):
+        response = self.client.get('/favourites-movies/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_request_user(self):
+        self.client.force_login(user=self.user)
         response = self.client.get('/favourites-movies/')
         self.assertEqual(response.status_code, 200)
 
     def test_favourite_list(self):
         pass
+
+
+class TestRateMovie(TestCase):
+    def setUp(self):
+        Movie.objects.create(title='abc')
+        Movie.objects.create(title='def')
+        Movie.objects.create(title='ghi')
+        self.username = 'testtest'
+        self.password = 'password123'
+        self.user = User.objects.create_user(self.username, self.password)
+
+    def test_request_anonymous_user(self):
+        response = self.client.get('/rate/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_request_user(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get('/rate/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_rate_movie_post(self):
+        self.client.force_login(user=self.user)
+        response = self.client.post('/rate/', {"rating": 4, "movie": 1})
+        self.assertEqual(response.status_code, 201)
+        response = self.client.post('/rate/', {"rating": 5, "movie": 1})
+        self.assertEqual(response.status_code, 400)
+        response = self.client.post('/rate/', {"rating": 7, "movie": 2})
+        self.assertEqual(response.status_code, 201)
+        response = self.client.post('/rate/', {"rating": -1, "movie": 3})
+        self.assertEqual(response.status_code, 400)
+        response = self.client.post('/rate/', {"rating": 11, "movie": 3})
+        self.assertEqual(response.status_code, 400)
+        response = self.client.post('/rate/', {"rating": 'asd', "movie": 3})
+        self.assertEqual(response.status_code, 400)
+        response = self.client.post('/rate/', {"rating": 4, "movie": 5})
+        self.assertEqual(response.status_code, 400)
+        self.client.logout()
+        user_2 = User.objects.create_user(username='testtest2', password='abcderfgh')
+        self.client.force_login(user=user_2)
+        response = self.client.post('/rate/', {"rating": 4, "movie": 1})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(MovieRating.objects.filter(user=self.user).count(), 2)
+        self.assertEqual(MovieRating.objects.filter(user=user_2).count(), 1)
+
+
+class TestMovieRateDetail(TestCase):
+    def setUp(self):
+        movie_first = Movie.objects.create(title='abc')
+        movie_second = Movie.objects.create(title='def')
+        movie_third = Movie.objects.create(title='ghi')
+        self.username = 'testtest'
+        self.password = 'password123'
+        self.user = User.objects.create_user(self.username, self.password)
+        user_2 = User.objects.create_user(username='testtest2', password='12345678')
+        MovieRating.objects.create(rating=8, movie=movie_first, user=self.user)
+        MovieRating.objects.create(rating=7, movie=movie_second, user=self.user)
+        MovieRating.objects.create(rating=1, movie=movie_third, user=user_2)
+
+    def test_request_anonymous_user(self):
+        response = self.client.get('/rate/1/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.put('/rate/1/', {"rating": 5, "movie": 1})
+        self.assertEqual(response.status_code, 403)
+
+    def test_request_user(self):
+        self.client.force_login(user=self.user)
+        response = self.client.put('/rate/1/', {"rating": '5', "movie": 1}, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.put('/rate/1/', {"rating": -1, "movie": 1}, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        response = self.client.put('/rate/1/', {"rating": 11, "movie": 1}, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        response = self.client.put('/rate/1/', {"rating": 'avd', "movie": 1}, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        response = self.client.put('/rate/3/', {"rating": 5, "movie": 3}, content_type='application/json')
+        self.assertEqual(response.status_code, 403)
+        response = self.client.delete('/rate/2/')
+        self.assertEqual(response.status_code, 204)
+        response = self.client.delete('/rate/3/')
+        self.assertEqual(response.status_code, 403)
+
+
+
+
+
+
